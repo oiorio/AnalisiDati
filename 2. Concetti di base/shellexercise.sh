b650b8d44c1c669d78f541294b0c2da42fa0471c
oiorio <oiorio@cern.ch>
2021-10-31 11:03:23 +0100

    Add files via upload

---
+++ 2. Concetti di base/shellexercise.sh
@@ -0,0 +1,17 @@
+echo "Hello world! 
+il comando echo ripete qualsiasi cosa tu scriva dopo, quindi espande le variabili 
+il path è"
+echo $PATH
+echo "un comando interessante è 'more' che fa leggere un file di testo dall'inizio alla fine, ad es.
+more shellexercise.sh. Adesso lancerò il comando:"
+#
+#
+more shellexercise.sh
+#
+#Ì commenti sono con il cancelletto. Questo commento compare perchè lo script usa il "more" 
+echo "altri comandi utili: 
+history : fa la storia dei comandi usati
+tail : legge un file dalla fine invece che dall'inizio
+ps : mostra i processi aperti sulla macchina
+"
+
