1831d454c06bcc695c3fade15f2da0b352051017
oiorio <oiorio@cern.ch>
2021-11-10 18:51:06 +0100

    Update launch_ll_fits.sh

--- 4. Fitting e massima verosimiglianza/launch_ll_fits.sh
+++ 4. Fitting e massima verosimiglianza/launch_ll_fits.sh
@@ -1,6 +1,6 @@
 source compile_likelihood.sh
 mkdir figures
 cd figures
-../NLL 1 ../macros/ 2D,profile
+../NLL 1 ../models/ 2D,profile
 
 #Domanda: come potrei fare se volessi lanciare NLL da qualsiasi posto del mio computer?
