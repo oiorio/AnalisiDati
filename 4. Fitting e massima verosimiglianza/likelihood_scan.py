693e6bd85e2f42b8e95c3d452f47afa3fe8428d6
oiorio <oiorio@cern.ch>
2023-11-29 10:24:02 +0100

    Update durante l'esercitazione

--- 4. Fitting e massima verosimiglianza/likelihood_scan.py
+++ 4. Fitting e massima verosimiglianza/likelihood_scan.py
@@ -82,7 +82,7 @@ nll_values.Write()
 #ora dobbiamo considerare due loop: sul parametro s e sul parametro b
 pois=ROOT.TF1("Nevents","TMath::Poisson(x,[0])",0,n_eventi)
 
-nsplit=10.0
+nsplit=5.0
 
 nll_values_2D_counting = ROOT.TH2F("nll_values_2D_counting","nll_values_2D_counting",n_eventi,0,n_eventi, n_eventi, 0, n_eventi)
 nll_values_2D_shape = ROOT.TH2F("nll_values_2D_shape","nll_values_2D_shape",int(n_eventi/nsplit),0,n_eventi, int(n_eventi/nsplit), 0, n_eventi)
@@ -97,7 +97,7 @@ for s in range(0,n_eventi,int(nsplit)):
         nll_poisson=0
         if(pois.Eval(n_eventi)!=0):
             nll_poisson=-2*math.log(pois.Eval(n_eventi))
-            nll_values_2D_counting.SetBinContent(s+1,b+1,-2*math.log(pois.Eval(n_eventi)))
+            nll_values_2D_counting.SetBinContent(int((s+1)/nsplit),int((b+1)/nsplit),-2*math.log(pois.Eval(n_eventi)))
         else:
             nll_poisson=0
             nll_values_2D_counting.SetBinContent(s+1,b+1,0)
@@ -114,17 +114,58 @@ for s in range(0,n_eventi,int(nsplit)):
 
         if(nll_poisson!=0):
             nll_tot= nll+nll_poisson
-            nll_values_2D_shape.SetBinContent(s+1,b+1,nll_tot)
+            nll_values_2D_shape.SetBinContent(int((s+1)/nsplit),int((b+1)/nsplit),nll_tot)
             print("nlltot is ", nll_tot)
         else:
             nll_values_2D_shape.SetBinContent(s+1,b+1,0)
-        
+
+nll_values_2D_counting.Draw("colZ")
+c1.SaveAs("nll_2D_counting.png")
+nll_values_2D_shape.Draw("colZ")
+c1.SaveAs("nll_2D_shape.png")
 nll_values_2D_counting.Write()
 nll_values_2D_shape.Write()
 
+#parte binnata, i dati sono xbinned_array
 
+nsplit=10.0 
 
+pois_bin = ROOT.TF1("TMath::Poisson(x,[0])",0,nevents)
 
+nll_values_2D_shape_binned = ROOT.TH2F("nll_values_2D_shape_binned","nll_values_2D_shape_binned",int(n_eventi/nsplit),0,n_eventi, int(n_eventi/nsplit), 0, n_eventi)
+
+for s in range(0,n_eventi,int(nsplit)):
+    for b in range(0,n_eventi,int(nsplit)):
+        #per valutare la likelihood binned come possiamo fare?
+
+        #se vogliamo considerare l'extended nll dobbiamo usare anche la funzione:
+        fgefrac.SetParameters(800,50,200,s,b)
+
+        nll=0
+        for i in range(len(xbinned_array)): 
+        #questo fa le veci del loop sugli eventi:
+        #for xi in x_array:
+        #    value_xi = fgefrac.Eval(xi)
+            x_data= xbinned[i]
+            x_i_min= h3.GetBinLowEdge(i)
+            x_i_max= h3.GetBinLowEdge(i+1)
+            epsilon_i=fgefrac.Integral(x_i_min, x_i_max)
+            nu_i=(s+b)*epsilon_i
+            pois_bin.SetParamter(0,nu_i)  # qui ci vuole epsilon_i * s+b#)
+            pois_i = pois_bin.Eval(x_data)
+            nll_i = 0
+            if pois_i != 0:
+                nll_i = -2*math.log(pois_i)
+            else :
+                nll_i = 10000
+            if(s==80 and b ==770):
+                print(" bin is ",i, " xmin ", x_i_min, " xmax ", x_i_max, " nu is ", nu_i, " epsilon ", epsilon_i, )
+            nll= nll+nll_i
+        nll_values_2D_shape_binned.SetBinContent(int((s+1)/nsplit),int((b+1)/nsplit),nll)            
+
+nll_values_2D_shape_binned.Draw()
+c1.SaveAs("nll_values_2D_shape_binned.png")
+nll_values_2D_shape_binned.Write()
 out_content.Close()
     #break
 
